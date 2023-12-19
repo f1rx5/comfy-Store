a5f42334984b96c0c21f49d354efacbfb2b72fe0
Firas Sharief <95207263+f1rx5@users.noreply.github.com>
2023-12-19 14:26:15 +0530

    v1

---
+++ src/pages/About.jsx
@@ -0,0 +1,27 @@
+const About = () => {
+  return (
+    <>
+      <div className="flex flex-wrap gap-2 sm:gap-x-6 items-center justify-center">
+        <h1 className="text-4xl font-bold leading-none tracking-tight sm:text-6xl">
+          We Love
+        </h1>
+        <div className="stats bg-primary shadow">
+          <div className="stat">
+            <div className="stat-title text-primary-content text-4xl font-bold tracking-widest">
+              Comfy
+            </div>
+          </div>
+        </div>
+      </div>
+      <p className="mt-6 text-lg leading-8 max-w-2xl mx-auto">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+        deserunt cumque ad beatae mollitia illum corporis libero, earum
+        similique officia dolores itaque reprehenderit quibusdam neque at quia
+        quos expedita illo non? Nemo iste, dolore laborum doloribus recusandae
+        necessitatibus nulla repudiandae exercitationem corporis laudantium
+        accusantium quo architecto officia cumque deleniti obcaecati?
+      </p>
+    </>
+  );
+};
+export default About;
